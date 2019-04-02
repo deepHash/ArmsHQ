5030f575cb867b39291b4be80c77465de904fb1e
Kaktus <itsesisx@gmail.com>
2019-04-02 18:05:47 +0300

    reworked DB

--- Server/database.js
+++ Server/database.js
@@ -1,13 +1,34 @@
-const redis = require("redis"),
-      client = redis.createClient();
+//--------------------------------Connect to mongodb on main DB via Mongoose--------------------------------//
+const mongoose = require('mongoose');
+      mongoose.Promise = global.Promise;
+      mongoose.set('useFindAndModify', false);
+//The server option auto_reconnect is defaulted to true
+var options = {
+    auto_reconnect:true
+};
 
-//open redis connection
-client.on('connect', function() {
-    console.log('Redis client connected');
-});
-//on redis error
-client.on('error', function (err) {
-    console.log('Something went wrong ' + err);
-});
+//-----------------------------------------------Test || Production-----------------------------------------//
+var Production = false;
+if (!Production){
+    mongoose.connect('mongodb://db_user:Arms123@ds237748.mlab.com:37748/arms', options);
+}
+else
+    mongoose.connect('mongodb://localhost:27017/arms', options);
+//
 
-module.exports = client;
+const conn = mongoose.connection;//get default connection
+
+// Event handlers for Mongoose
+conn.on('error', function (err) {
+    console.log('Mongoose: Error: ' + err);
+});
+conn.on('open', function() {
+    console.log('Mongoose: Connection established');
+});
+conn.on('disconnected', function() {
+    console.log('Mongoose: Connection stopped, recconect');
+    mongoose.connect('mongodb://localhost:27017/main', options);
+});
+conn.on('reconnected', function () {
+    console.info('Mongoose reconnected!');
+});
---
+++ Server/models/Soldier.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+    schema   = mongoose.Schema,
+
+    SoldierSchema = new schema({
+        meshID: {type: Number, required: true},
+        name: String,
+        bloodType: String,
+        gps: {
+             lan: Number, 
+             lat: Number
+        },
+        acc: {
+             x: Number,
+             y: Number,
+             z: Number
+        },
+        pulse: Number,
+        emerg: Boolean,
+        messages: [{
+            messageID: Number,
+            data: {type: String,
+                   value: schema.Types.Mixed}}]
+    }, {collection: 'Soldier'});
+
+    var Soldier = mongoose.model('Soldier', SoldierSchema);
+    module.exports = Soldier;
